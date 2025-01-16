50b876055b02d1323d58d46a15ca64a6aa2c3237
TcePrepK <a.s.arda.simsek@gmail.com>
2025-01-17 01:22:02 +0300

    First window and WebGPU

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use crate::core::window;
+
+mod core;
+
+#[cfg(target_arch="wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
+pub fn run() {
+    window::init();
+}
